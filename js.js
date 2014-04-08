62ff3023a36368a1a3e1cfc2e647b3bfe2d4bda1
Simon Rai <simon@raize.ca>
2014-04-08 19:31:40 +0000

    added bootsrap elements and basic structure

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 $(function() {
-    $('#save-btn').click(function() {
-        var input = $("#full-name");
-        var data = input.val();
-        $('#output').append("<p>" + data +"</p>");
-    });
+  $('#save-btn').click(function() {
+    var input = $("#full-name");
+    var data = input.val();
+    $('#output').append("<p>" + data +"</p>");
+  });
 });
