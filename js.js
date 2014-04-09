6324889d3e17424e2af410bc08e5cb7a1ab6119d
Simon Rai <simon@raize.ca>
2014-04-09 03:26:01 +0000

    item is removed if it recieves a click

--- js.js
+++ js.js
@@ -2,8 +2,11 @@ $(function() {
   $('#work-btn').click(function() {
     var input = $("#task");
     var data = input.val();
-    $('#work-tasks').append("<p>" + data + "</p>");
+    $('#work-tasks').append("<p class=\"delete\">" + data + "</p>");
     $('#task').val("");
+    $('#work-tasks').on('click', '.delete', function() {
+  	$(this).remove();
+	});
   });
 });
 
@@ -11,16 +14,22 @@ $(function() {
   $('#school-btn').click(function() {
     var input = $("#task");
     var data = input.val();
-    $('#school-tasks').append("<p>" + data +"</p>");
+    $('#school-tasks').append("<p class=\"delete\">" + data + "</p>");
     $('#task').val("");
+    $('#school-tasks').on('click', '.delete', function() {
+  	$(this).css("text-decoration", "line-through");
   });
+});
 });
 
 $(function() {
   $('#home-btn').click(function() {
     var input = $("#task");
     var data = input.val();
-    $('#home-tasks').append("<p>" + data +"</p>");
-    $('#task').val("");    
+    $('#home-tasks').append("<p class=\"delete\">" + data + "</p>");
+    $('#task').val("");
+    $('#home-tasks').on('click', '.delete', function() {
+  	$(this).remove();
   });
+});
 });
